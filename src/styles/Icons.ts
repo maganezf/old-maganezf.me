240784e4a252df97b320102dd6a5ac2e9f00cf56
Maganez Filho <maganezfilho@gmail.com>
2021-02-24 12:46:43 -0300

    Added Readme, License, Contributing, CodeOfConduct

--- src/styles/Icons.ts
+++ src/styles/Icons.ts
@@ -1,7 +1,7 @@
-export { WaveBackgroundImgDesktop } from '../assets/icons/WaveBackgroundImgDesktop';
-export { WaveBackgroundImgMobile } from '../assets/icons/WaveBackgroundImgMobile';
+export { WaveBackgroundImgDesktop } from '../public/icons/WaveBackgroundImgDesktop';
+export { WaveBackgroundImgMobile } from '../public/icons/WaveBackgroundImgMobile';
 
-export { CodeIcon } from '../assets/icons/CodeIcon';
-export { LineSeparatorIcon } from '../assets/icons/LineSeparatorIcon';
-export { MoonIconLightTheme } from '../assets/icons/MoonIconLightTheme';
-export { SunIconDarkTheme } from '../assets/icons/SunIconDarkTheme';
+export { CodeIcon } from '../public/icons/CodeIcon';
+export { LineSeparatorIcon } from '../public/icons/LineSeparatorIcon';
+export { MoonIconLightTheme } from '../public/icons/MoonIconLightTheme';
+export { SunIconDarkTheme } from '../public/icons/SunIconDarkTheme';
